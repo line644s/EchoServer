8577dd628e725bdabe7be96424cf83ef197802b3
Line Phoebe Wienke <line644s@edu.easj.dk>
2019-08-29 11:00:41 +0200

    Opgave C Client

---
+++ Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker client = new Worker();
+            client.start();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Client/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace EchoClient
+{
+    class Worker
+    {
+        public void start()
+        {
+            using (TcpClient socket = new TcpClient("localhost", 7))
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            {
+                string str = Console.ReadLine();
+
+                sw.WriteLine(str);
+                sw.Flush();
+
+                string strin = sr.ReadLine();
+                Console.WriteLine(strin);
+            }
+            
+        }
+    }
+}
